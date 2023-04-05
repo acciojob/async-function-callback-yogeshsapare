4a314a919fcc748986de2b7a62647564ea54cad8
yogeshsapare <117711304+yogeshsapare@users.noreply.github.com>
2023-04-05 23:00:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-const url = "https://jsonplaceholder.typicode.com/posts/1";
+const apiUrl = "https://jsonplaceholder.typicode.com/posts/1";
 
 //your JS code here. If required.
+
+async function getData(callback) {
+  const response = await fetch(apiUrl);
+  const data = await response.json();
+  callback(data.title);
+}
+
+document.getElementById("btn").addEventListener("click", async function() {
+  getData((title) => {
+    document.getElementById("output").innerHTML = title;
+  });
+});
